8dd8ddbbbd1a72b6bf2fe8005b10133e09527629
Tom Byers <tombaromba@gmail.com>
2014-02-10 10:21:58 +0000

    Explain locales env' vars in the provision script

--- provision_design_patterns_vm.sh
+++ provision_design_patterns_vm.sh
@@ -6,6 +6,8 @@ apt-get -y install ruby1.9.1-dev
 apt-get -y install make
 
 # Set language environment variables to UTF-8
+# There are non-ascii characters in our .md files
+# Kramdown (which Jekyll uses to parse Markdown) breaks without these set
 echo "Exporting environment variables"
 echo "export LANG=en_GB.utf8" >> /home/vagrant/.bashrc
 echo "export LC_ALL=en_GB.utf8" >> /home/vagrant/.bashrc
